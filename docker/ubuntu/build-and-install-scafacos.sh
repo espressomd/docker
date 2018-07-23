4044848cf5e668ee523a355924434c3c95edfb3d
Rudolf Weeber <rudolf.weeber@gmx.de>
2018-07-23 11:42:44 +0200

    Update scafacos build script (upstream url + dipole support)

--- docker/ubuntu/build-and-install-scafacos.sh
+++ docker/ubuntu/build-and-install-scafacos.sh
@@ -1,10 +1,10 @@
 cd /tmp &&
-git clone git://github.com/RudolfWeeber/scafacos --branch for_use_with_espresso_dipoles &&
+git clone git://github.com/scafacos/scafacos --branch dipoles &&
 cd scafacos &&
 git submodule init &&
 git submodule update &&
 ./bootstrap &&
-./configure --with-internal-pfft --with-internal-pnfft --enable-fcs-solvers=direct,pnfft,p2nfft,p3m --enable-shared --disable-fcs-fortran &&
+./configure --with-internal-pfft --with-internal-pnfft --enable-fcs-solvers=direct,pnfft,p2nfft,p3m --enable-shared --disable-fcs-fortran --enable-fcs-dipoles &&
 make -j `nproc` &&
 make install &&
 cd &&
