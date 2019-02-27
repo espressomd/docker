301c742031ebedd6f6728388917bc52c4da9955b
Kai Szuttor <kai@icp.uni-stuttgart.de>
2019-02-27 10:21:13 +0100

    Correct error handling in container build script.

--- maintainer/docker_build.sh
+++ maintainer/docker_build.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
-# execute and output a command
-function cmd {
-    echo ">$1"
-    eval $1
+abort()
+{
+    echo "An error occurred. Exiting..." >&2
+    echo "Command that failed: $BASH_COMMAND" >&2
+    exit 1
 }
 
-cmd "cd docker/${CI_JOB_NAME%:*}"
+trap 'abort' 0
+set -ex
+
+cd docker/${CI_JOB_NAME%:*}
 echo "ACTIVATION_LICENSE_FILE=$INTEL_LICENSE_SERVER" >> intel.cfg
-cmd "test -f Dockerfile-${CI_JOB_NAME#*:}" || ./generate.sh ${CI_JOB_NAME#*:}
-cmd "docker build -f Dockerfile-${CI_JOB_NAME#*:}* --pull -t $CI_REGISTRY/$CI_PROJECT_PATH/test/$CI_JOB_NAME-$CI_COMMIT_SHA --cache-from $CI_REGISTRY/$CI_PROJECT_PATH/$CI_JOB_NAME ."
-cmd "docker push $CI_REGISTRY/$CI_PROJECT_PATH/test/$CI_JOB_NAME-$CI_COMMIT_SHA"
+test -f Dockerfile-${CI_JOB_NAME#*:} || ./generate.sh ${CI_JOB_NAME#*:}
+docker build -f Dockerfile-${CI_JOB_NAME#*:}* --pull -t $CI_REGISTRY/$CI_PROJECT_PATH/test/$CI_JOB_NAME-$CI_COMMIT_SHA --cache-from $CI_REGISTRY/$CI_PROJECT_PATH/$CI_JOB_NAME .
+docker push $CI_REGISTRY/$CI_PROJECT_PATH/test/$CI_JOB_NAME-$CI_COMMIT_SHA
 test "$CI_COMMIT_REF_NAME" != "master" || docker tag $CI_REGISTRY/$CI_PROJECT_PATH/test/$CI_JOB_NAME-$CI_COMMIT_SHA $CI_REGISTRY/$CI_PROJECT_PATH/$CI_JOB_NAME
 test "$CI_COMMIT_REF_NAME" != "master" || docker push $CI_REGISTRY/$CI_PROJECT_PATH/$CI_JOB_NAME
+
+trap : 0
