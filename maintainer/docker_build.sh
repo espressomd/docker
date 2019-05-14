55d12514cf40901e8454189663f1ba14435cbf95
Michael Kuron <mkuron@users.noreply.github.com>
2019-05-14 11:11:32 +0200

    Fix building of exotic architecture containers

--- maintainer/docker_build.sh
+++ maintainer/docker_build.sh
@@ -13,7 +13,7 @@ function cmd {
 cmd "cd docker/${CI_JOB_NAME%:*}"
 echo "ACTIVATION_LICENSE_FILE=$INTEL_LICENSE_SERVER" >> intel.cfg
 echo "ACTIVATION_LICENSE_FILE=$INTEL_LICENSE_SERVER" >> intel-15.cfg
-cmd "test -f Dockerfile-${CI_JOB_NAME#*:}" || ./generate.sh ${CI_JOB_NAME#*:}
+test -f Dockerfile-${CI_JOB_NAME#*:} || cmd "./generate.sh ${CI_JOB_NAME#*:}"
 cmd "docker build -f Dockerfile-${CI_JOB_NAME#*:}* --pull -t $CI_REGISTRY/$CI_PROJECT_PATH/test/$CI_JOB_NAME-$CI_COMMIT_SHA --cache-from $CI_REGISTRY/$CI_PROJECT_PATH/$CI_JOB_NAME ."
 cmd "docker push $CI_REGISTRY/$CI_PROJECT_PATH/test/$CI_JOB_NAME-$CI_COMMIT_SHA"
 test "$CI_COMMIT_REF_NAME" != "master" || cmd "docker tag $CI_REGISTRY/$CI_PROJECT_PATH/test/$CI_JOB_NAME-$CI_COMMIT_SHA $CI_REGISTRY/$CI_PROJECT_PATH/$CI_JOB_NAME"
