550442a27de5428ba712ad7cc8cb22f856790418
Kai Szuttor <kai@icp.uni-stuttgart.de>
2020-04-03 13:54:19 +0200

    push image to dockerhub if credentials are provided.

--- .github/actions/docker/entrypoint.sh
+++ .github/actions/docker/entrypoint.sh
@@ -7,6 +7,8 @@ project=$(echo "$2" | cut -d'/' -f2)
 password=$3
 event_name=$4
 tag=$5
+dockerhub_user=$6
+dockerhub_password=$7
 
 echo "Log in to registry."
 echo $password | docker login -u ${username} --password-stdin docker.pkg.github.com || exit 1
@@ -16,7 +18,12 @@ docker build docker -t docker.pkg.github.com/${username}/${project}/${image}:${t
 
 if [ "$event_name" != "pull_request" ]; then
     echo "Pushing to registry."
-    docker push docker.pkg.github.com/${username}/${project}/${image}:${tag}
+    docker push docker.pkg.github.com/${username}/${project}/${image}:${tag} || exit 1
+    if [ "$dockerhub_user" != "" ]; then
+        echo $dockerhub_password | docker login -u ${dockerhub_user} --password-stdin || exit 1
+        docker tag docker.pkg.github.com/${username}/${project}/${image}:${tag} ${username}/${project}-${image}:${tag}
+        docker push ${username}/${project}-${image}:${tag} || exit 1
+    fi  
 else
     echo "Pull request: not pushing to registry."
 fi
