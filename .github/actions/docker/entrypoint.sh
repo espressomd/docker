f55dcbfdc54e4204b5a2aafaa9cec5609919611f
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2023-04-04 20:24:36 +0200

    Generate images suitable for GitHub Actions
    GitHub Actions require images without any custom USER or WORKDIR:
    https://docs.github.com/en/actions/creating-actions/dockerfile-support-for-github-actions

--- .github/actions/docker/entrypoint.sh
+++ .github/actions/docker/entrypoint.sh
@@ -12,13 +12,25 @@ echo "Log in to registry."
 echo "${password}" | docker login -u "${username}" --password-stdin docker.pkg.github.com || exit 1
 
 full_tag="docker.pkg.github.com/${username}/${project}/${image}:${tag}"
+dockerfile="docker/Dockerfile-${image}"
+full_tag_github_action=""
+if grep -q "FROM image_base AS image_standalone" "${dockerfile}"; then
+  full_tag_github_action="${full_tag}-base-layer"
+fi
 
 echo "Building image with tag: ${full_tag}"
-docker build docker -t "${full_tag}" -f "docker/Dockerfile-${image}" || exit 1
+docker build docker -t "${full_tag}" -f "${dockerfile}" || exit 1
+if [ ! -z "${full_tag_github_action}" ]; then
+  echo "Building GitHub Action-specific image with tag: ${full_tag_github_action}"
+  docker build docker -t "${full_tag_github_action}" -f "${dockerfile}" --target image_base || exit 1
+fi
 
 if [ "${event_name}" = "pull_request" ]; then
     echo "Pull request: not pushing to registry."
 else
     echo "Pushing to registry."
     docker push "${full_tag}" || exit 1
+    if [ ! -z "${full_tag_github_action}" ]; then
+      docker push "${full_tag_github_action}" || exit 1
+    fi
 fi
